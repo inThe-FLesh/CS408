d6108bc85075e7ace72ffd9966e8369afa69319e
inThe-FLesh <rgrayy0099@gmail.com>
2023-10-28 15:42:19 +0100

    Created cpp files for cpu hashing algorithm and implmented string_to_binary and partially added pad_binary

---
+++ SHA256.h
@@ -0,0 +1,20 @@
+//
+// Created by ross on 27/10/23.
+//
+
+#ifndef CS408_SHA256_H
+#define CS408_SHA256_H
+#include <iostream>
+#include <iomanip>
+#include <bitset>
+#include <cmath>
+
+using namespace std;
+
+
+class SHA256 {
+
+};
+
+
+#endif //CS408_SHA256_H
