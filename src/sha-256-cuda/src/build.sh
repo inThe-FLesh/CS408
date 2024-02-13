f4fe50ef74f0961a9c3417a10f41d95b9e9110af
inThe-FLesh <rgrayy0099@gmail.com>
2024-02-13 22:56:17 +0000

    cuda changes

--- src/sha-256-cuda/src/build.sh
+++ src/sha-256-cuda/src/build.sh
@@ -1,1 +1,2 @@
-nvcc -gencode arch=compute_75,code=sm_75 -rdc=true SHA256-Cuda.cu preprocessing.cu computation.cu -o main
+nvcc -gencode arch=compute_75,code=sm_75 -rdc=true SHA256-Cuda.cu -o main 
+./main
