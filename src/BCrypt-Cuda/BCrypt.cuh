91680aba17b4fde5f4333a2c792ef6cf5b7cfde9
inThe-FLesh <rgrayy0099@gmail.com>
2024-03-14 02:42:05 +0000

    set up BCrypt-Cuda

---
+++ src/BCrypt-Cuda/BCrypt.cuh
@@ -0,0 +1,15 @@
+#include "EksBlowfish.cuh"
+#include "base64.cuh"
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <iomanip>
+#include <string>
+#include <sys/time.h>
+#include <sys/types.h>
+
+using std::cout;
+using std::endl;
+
+int BCrypt(int cost, const uint8_t *salt, uint8_t *password,
+           int passwordLength);
---
+++ src/BCrypt-Cuda/cmake-build-debug/CMakeFiles/BCrypt.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ross/Documents/Year4/CS408/CS408/src/BCrypt/BCrypt.cpp" "/home/ross/Documents/Year4/CS408/CS408/src/BCrypt/cmake-build-debug/CMakeFiles/BCrypt.dir/BCrypt.cpp.o"
+  "/home/ross/Documents/Year4/CS408/CS408/src/BCrypt/base64.cpp" "/home/ross/Documents/Year4/CS408/CS408/src/BCrypt/cmake-build-debug/CMakeFiles/BCrypt.dir/base64.cpp.o"
+  "/home/ross/Documents/Year4/CS408/CS408/src/BCrypt/test.cpp" "/home/ross/Documents/Year4/CS408/CS408/src/BCrypt/cmake-build-debug/CMakeFiles/BCrypt.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ross/Documents/Year4/CS408/CS408/src/BCrypt/."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
