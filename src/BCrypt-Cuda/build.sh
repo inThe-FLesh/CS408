5efdb33873a922ad9abceb5746fe6e6e39fa28f1
inThe-FLesh <rgrayy0099@gmail.com>
2024-03-19 20:07:08 +0000

    changed build scripts to not be architecture dependent

--- src/BCrypt-Cuda/build.sh
+++ src/BCrypt-Cuda/build.sh
@@ -1,2 +1,2 @@
-nvcc -gencode arch=compute_75,code=sm_75 -rdc=true base64.cu BCrypt.cu -o BCrypt
+nvcc base64.cu BCrypt.cu -o BCrypt
 ./BCrypt
--- src/sha-256-cuda/build.sh
+++ src/sha-256-cuda/build.sh
@@ -1,2 +1,2 @@
-nvcc -gencode arch=compute_75,code=sm_75 -rdc=true SHA256-Cuda.cu -o SHA256 
+nvcc SHA256-Cuda.cu -o SHA256 
 ./SHA256
